6716caec42890061c4ac8b6a7d62773dd4f430b6
ntscas <ntscas@gmail.com>
2026-06-16 23:01:32 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,51 @@
+const CACHE_NAME = 'tax-talk-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch((err) => console.log('[PWA] Precaching failed during install:', err));
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   console.log('[PWA] Service Worker Activated');
 });
 
 self.addEventListener('fetch', (e) => {
-  // Minimal fetch handler to satisfy Chrome PWA installability requirements
+  // Only target http/https scopes
+  if (!e.request.url.startsWith('http')) return;
+
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(e.request).then((networkResponse) => {
+        // Return clear response directly
+        return networkResponse;
+      }).catch(() => {
+        // Safe offline response instead of crashing
+        return caches.match('/');
+      });
+    })
+  );
 });
+
